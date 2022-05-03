d718e85627de3c4cecf785328641353c882f46c2
Wilson Andrés Pinillo <101427568+WPinillo@users.noreply.github.com>
2022-05-03 18:32:18 -0500

    Update pregunta_14.sql

--- pregunta_14.sql
+++ pregunta_14.sql
@@ -44,15 +44,15 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
 SELECT
-    t1.K0,
+    t1.k0,
     AVG(t2.c21)
 FROM
     tbl1 t1
-INNER JOIN
+LEFT JOIN
     tbl2 t2
 ON
-    (t1.K1 = t2.K1)
+    (t1.k1 = t2.k1)
 WHERE
     t1.c13 > 400
 GROUP BY
-    K0
+    k0
